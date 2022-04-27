8b2b66e01d1a92ff23e1466a7bfd0846c2052fe8
ValcirJr <balbinottijr@outlook.com>
2022-04-27 20:19:25 -0300

    aula 20/04

--- customer/src/main/java/br/edu/utfpr/FraudClient.java
+++ customer/src/main/java/br/edu/utfpr/FraudClient.java
@@ -1,14 +1,12 @@
 package br.edu.utfpr;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("${customer.fraud.name}")
+@FeignClient("${customer.fraud.url}")
 public interface FraudClient {
 
     @PostMapping(value = "fraud/v1/fraud-check/{customerId}")
     FraudCheckResponse checkFraud(@PathVariable("customerId") Integer customerId);
-
 }
